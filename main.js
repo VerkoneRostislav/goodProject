73dd087ffbe3914bea283a3a09e84877aa0e7ecc
VerkoneRostislav <r.zaharov2011@gmail.com>
2024-09-23 17:50:08 +0200

    SSEStelefon

--- main.js
+++ main.js
@@ -28,10 +28,25 @@ window.onscroll = function() {
  else{
    classLink = '.main-link';
  }
- let activeBtn = document.querySelector('.active');
- let newActiveBtn = document.querySelector(classLink);
- if (!newActiveBtn.classList.contains('active')){
-   newActiveBtn.classList.add('active');
-   activeBtn.classList.remove('active')
- }
-};
+}
+
+document.querySelector(".mobile-button").addEventListener("click",
+function(event)
+{
+  document.querySelector(".mobile-menu").classList.toggle("hide");
+});
+
+menu.addEventListener('click', function(event){
+  let clickTarget = event.target;
+  let activeBtn = document.querySelector('.active');
+  if (clickTarget.classList.contains('nav-link') ){
+      clickTarget.classList.add('active');
+      activeBtn.classList.remove('active');
+
+      let mobile = document.querySelector('.mobile-menu');
+      if (!mobile.classList.contains('hide')){
+          mobile.classList.add('hide');
+      }
+
+  }
+});
